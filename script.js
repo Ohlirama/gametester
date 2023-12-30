f617106d82646ee877e12d50f79897522608e666
Erick Oliveira Amarilho <131217547+Ohlirama@users.noreply.github.com>
2023-12-30 01:28:55 -0300

    opaoladf

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
         saveNotes('avaliacao', avaliacaoInput.value);
         calculateAndSetAverage();
     });
+
+    // Adiciona listener para o botão de Avaliação Final
+    const finalButton = document.getElementById('final-button');
+    finalButton.addEventListener('click', avaliacaoFinal);
 });
 
 function addSectionListeners() {
@@ -27,66 +31,31 @@ function addSectionListeners() {
 }
 
 function calculateAndSetAverage() {
-    // Pega todas as seções de notas
-    const sections = document.querySelectorAll('section');
-
-    // Inicializa variáveis para a soma e contagem de notas
-    let total = 0;
-    let count = 0;
-
-    // Itera sobre cada seção
-    sections.forEach(section => {
-        const savedNotes = localStorage.getItem(section.id);
-
-        // Se a nota não for vazia
-        if (savedNotes) {
-            // Converte a nota para um número
-            const note = parseFloat(savedNotes);
-
-            // Se a nota for um número válido
-            if (!isNaN(note)) {
-                total += note;
-                count++;
-            }
-        }
-    });
-
-    // Pega a avaliação de 0 a 10
-    const avaliacao = parseFloat(localStorage.getItem('avaliacao')) || 0;
-
-    // Se a avaliação de 0 a 10 for válida, a inclui no cálculo da média
-    if (!isNaN(avaliacao)) {
-        total += avaliacao;
-        count++;
-    }
-
-    // Calcula a média
-    const average = count === 0 ? 0 : total / count;
-
-    // Seleciona o elemento onde exibir a média
-    const averageDisplay = document.getElementById('average-display');
-
-    // Exibe a média com uma casa decimal
-    averageDisplay.textContent = `Média: ${average.toFixed(1)}`;
-
-    // Seleciona o elemento onde exibir a cor
-    const colorDisplay = document.getElementById('color-display');
+    // ... (mesmo código)
 
     // Define a cor do texto com base na média
-    colorDisplay.style.color = average >= 5 ? 'green' : 'red';
+    const colorDisplay = document.getElementById('color-display');
+    colorDisplay.style.color = getTextColor(average);
+
+    // Atualiza o texto do botão de Avaliação Final
+    const finalButton = document.getElementById('final-button');
+    finalButton.style.backgroundColor = getBackgroundColor(average);
 }
 
-function saveNotes(sectionId, notes) {
-    // Salva as anotações no Local Storage
-    localStorage.setItem(sectionId, notes);
+function getTextColor(average) {
+    return average > 5 ? 'green' : (average < 5 ? 'red' : 'black');
+}
+
+function getBackgroundColor(average) {
+    return average > 5 ? 'green' : (average < 5 ? 'red' : 'yellow');
 }
 
 function avaliacaoFinal() {
     const avaliacao = parseFloat(localStorage.getItem('avaliacao')) || 0;
     const mensagemFinalElement = document.getElementById('mensagem-final');
 
-    // Remove o botão e exibe a mensagem final
-    const finalButton = document.querySelector('button');
+    // Remove o botão
+    const finalButton = document.getElementById('final-button');
     finalButton.remove();
 
     // Adiciona a mensagem final com base na avaliação
